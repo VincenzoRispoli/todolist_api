814fcb1ffb8b9771bdb00e7997c16ef5c8da4a7e
VincenzoRispoli <150629139+VincenzoRispoli@users.noreply.github.com>
2024-09-27 11:34:08 +0200

    adjustment

--- todolist_api/urls.py
+++ todolist_api/urls.py
@@ -22,7 +22,7 @@ router = routers.DefaultRouter()
 router.register(r'todos', views.TodoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
