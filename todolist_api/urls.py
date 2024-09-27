17cd22731d18a6645fad189b62db2ce04219c97a
VincenzoRispoli <150629139+VincenzoRispoli@users.noreply.github.com>
2024-09-27 11:36:16 +0200

    new

--- todolist_api/urls.py
+++ todolist_api/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from todo import views
+from todo.views import TodoViewSet
 
 router = routers.DefaultRouter()
-router.register(r'todos', views.TodoViewSet)
+router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
